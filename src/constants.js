da56d142a1fc86dafd7dca6005ae621b0cce0e3a
Chett Tiller <chett.tiller@gmail.com>
2019-09-04 11:43:19 -0400

    Got rid of a few constants

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,11 @@
 // Literally just the name being used by the app in case it should change
 export const appName = 'Board Game Recommender'
-// Endpoint going to flask backend
+
+// Endpoint going to current flask backend
 export const dbEndpoint = 'https://361a6d43.ngrok.io/rec'
-// Endpoint for names db
-export const localEndpoint = 'http://localhost:3100'
+
 // Rice paddies and industry background
 // export const defaultGameBackground = 'https://cdn.theatlantic.com/assets/media/img/2017/11/01/WEL_Andersen_ChinaSETI_Web_Lead-1/1920.jpg?1509571481'
+
 // Catan background
 export const defaultGameBackground = 'https://s3.amazonaws.com/walrus-assets/img/BoardGameTheory.jpg'
-// Ancient Go Background
-// export const defaultGameBackground = 'https://wellingtongoclub.nz/images/ancient_go.jpg'
-// Chinese Watercolor
-// export const defaultGameBackground = 'https://dygtyjqp7pi0m.cloudfront.net/i/10441/11100693_1.jpg?v=8CE317A73088D10'
