d14bdc552960ca58082fb28e278cd4aad8f5d45b
Chett Tiller <chett.tiller@gmail.com>
2019-09-04 12:03:29 -0400

    Minor styling changes and notes

--- src/components/BoardGameCollection/BoardGameStars.js
+++ src/components/BoardGameCollection/BoardGameStars.js
@@ -6,12 +6,13 @@ const BoardGameStars = ({rating}) => {
     return `${(rating / 10) * 5}em`
   }
 
-  // ✩✩✩✩✩ ❀ ✿ ⭑
+  // Possible unicode symbols: ✩✩✩✩✩ ❀ ✿ ⭑
+  // &#xFE0E; ensure it uses proper unicode rather than emoji
 
   return (
     <div className='stars-container'>
       <p className='stars-underlay'>✿✿✿✿✿</p>
-      <p className='stars-overlay' style={{width: calculateWidth()}}>✿✿✿✿✿</p>
+      <p className='stars-overlay' style={{width: calculateWidth()}}>✿&#xFE0E;✿&#xFE0E;✿&#xFE0E;✿&#xFE0E;✿&#xFE0E;</p>
     </div>
   )
 }
